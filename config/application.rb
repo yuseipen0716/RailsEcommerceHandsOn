47caf38e5f19aa2c0254f2061d7b7aecc21dfa40
yuseipen0716 <yuseipen1@gmail.com>
2023-06-18 02:59:10 +0900

    rails generateでassetsやrouteへの追加、testファイルの作成をskipするように修正

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,12 @@ module Hello
     origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "http:\/\/localhost*" }.split(",")
     origins.map! { |url| /#{url}/ }
     config.action_cable.allowed_request_origins = origins
+
+    # Customizing Raile Generators
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.test_framework false
+    end
   end
 end
