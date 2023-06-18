503bbe3773c38f054ef9a9e1e7a6e1c6f7b357af
yuseipen0716 <yuseipen1@gmail.com>
2023-06-18 11:21:38 +0900

    Customerモデルにバリデーションを追加。enumのsetup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :name
+    validates :status
+  end
+  enum status: {
+    normal: 0,
+    withdrawn: 1,
+    banned: 2
+  }
 end
