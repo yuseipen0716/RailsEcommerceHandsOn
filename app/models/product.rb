9a23e921e90b53b59f646db57e510eedb0efe505
yuseipen0716 <yuseipen1@gmail.com>
2023-06-20 01:27:07 +0900

    modelにscopeを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,7 @@ class Product < ApplicationRecord
   end
 
   has_one_attached :image
+
+  scope :price_high_to_low, -> { order(price: :desc) }
+  scope :price_low_to_high, -> { order(price: :asc) }
 end
